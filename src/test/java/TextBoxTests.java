2a6b73a0e99a834991ebba93d76abf8c786297d1
k_mm5 <lisenokk.27.12@gmail.com>
2024-01-02 19:06:03 +0300

    test 1

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static java.nio.channels.FileChannel.open;
 
 public class TextBoxTests {
 
@@ -13,14 +11,13 @@ public class TextBoxTests {
     static void beforeAll() {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://demoqa.com/text-box";
     }
 
 
     @Test
     void fillFromTest() {
-        
-        open("/text-box");
+
 
         $("#userName").setValue("Fox");
         $("#userEmail").setValue("Fox@mak.com");
@@ -30,6 +27,5 @@ public class TextBoxTests {
 
         $("#output").shouldBe(Condition.visible);
     }
-
 
 }
